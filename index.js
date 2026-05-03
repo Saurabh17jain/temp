6c1d437220457734c40943f1372d684080b8a190
Shamma18 <118926699+Shamma18@users.noreply.github.com>
2026-05-03 22:34:42 +0530

    fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/cpu", (req, res) => {
     for (let i = 0; i < 10000000000; i++) {
         Math.random();
     }
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 
 app.get("/host", (req, res) => {
