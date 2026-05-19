3137f839b3f5784fd1f6e7432e828870ee9a8fb3
Saurabh17jain <saurabh1712jain@gmail.com>
2026-05-19 23:36:29 +0530

    commit

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ import os from "os";
 export const app = express();
 
 app.get("/", (req, res) => {
-    res.send("How are you");
+  res.send("How are you!");
 });
 
 app.get("/cpu", (req, res) => {
-    for (let i = 0; i < 10000000000; i++) {
-        Math.random();
-    }
-    res.send("Hello World!");
+  for (let i = 0; i < 10000000000; i++) {
+    Math.random();
+  }
+  res.send("Hello World!");
 });
 
 app.get("/host", (req, res) => {
-    res.send(os.hostname());
+  res.send(os.hostname());
 });
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
